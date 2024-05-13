8b0e0eff3d7e46065559540d7756803b31101467
strozw <strozw@users.noreply.github.com>
2024-05-13 14:31:38 +0900

    biome

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import type * as CSS from "csstype";
+import type { CSSRuleObject } from "tailwindcss/types/config";
+
+export type CSSProperties = CSS.Properties<number | (string & {})>;
+
+export type CSSPropertiesHyphen = CSS.PropertiesHyphen<number | (string & {})>;
+
+export type CSSPseudos = { [K in CSS.Pseudos]?: StyleObject };
+
+export interface StyleObject
+	extends CSSProperties,
+		CSSPropertiesHyphen,
+		CSSPseudos {
+	[key: string]: StyleObject | string | number | undefined;
+}
+
+export function defineComponent(component: StyleObject) {
+	return component as CSSRuleObject;
+}
